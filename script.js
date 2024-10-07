907ea6eb81a861c021269b98616a8e250dc4626e
soheb25 <sohebansari252003@gmail.com>
2024-10-07 22:30:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.game-card').forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        card.style.transform = 'scale(1.05)';
+        card.style.transition = 'transform 0.3s ease';
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = 'scale(1)';
+    });
+});
